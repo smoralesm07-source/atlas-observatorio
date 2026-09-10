1888dbd0b4f07db295fa0b97de1dd7013e67823d
smoralesm07-source <smoralesm07@gmail.com>
2026-09-10 15:13:56 -0300

    Activa drawer flotante de Gestión SO

--- src/main.tsx
+++ src/main.tsx
@@ -1,6 +1,7 @@
 import { StrictMode } from 'react';
 import { createRoot } from 'react-dom/client';
 import App from './App';
+import './lib/caseDrawerEnhancer';
 import './styles/app.css';
 import './styles/explore.css';
 import './styles/osfl.css';
