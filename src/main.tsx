1220457224abd28eacd24813d5e58d00ab6e20fe
smoralesm07-source <smoralesm07@gmail.com>
2026-09-10 15:33:20 -0300

    Activa filtros interactivos del directorio territorial

--- src/main.tsx
+++ src/main.tsx
@@ -2,6 +2,7 @@ import { StrictMode } from 'react';
 import { createRoot } from 'react-dom/client';
 import App from './App';
 import './lib/caseDrawerEnhancer';
+import './lib/territoryDirectoryEnhancer';
 import './styles/app.css';
 import './styles/explore.css';
 import './styles/osfl.css';
