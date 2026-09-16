c0518d5e4bd6da4270391f59aed45f59f87f9233
smoralesm07-source <smoralesm07@gmail.com>
2026-09-16 17:18:24 -0300

    feat: activa informe estratégico con alcance sectorial

--- src/App.tsx
+++ src/App.tsx
@@ -15,7 +15,7 @@ import { Entidades } from './views/Entidades';
 import { EntityExpediente } from './views/EntityExpediente';
 import { Fuentes } from './views/Fuentes';
 import { Territorio } from './views/Territorio';
-import { ReportesDirectivosV2 } from './views/ReportesDirectivosV2';
+import { ReportesDirectivosV3 } from './views/ReportesDirectivosV3';
 import { Metodologia } from './views/Metodologia';
 import { Administracion } from './views/Administracion';
 import './styles/territory-commune-typography.css';
@@ -74,7 +74,7 @@ function Routed({ session, role }: { session: Session; role: AtlasRole }) {
         </>
       )}
       {route.view === 'territorio' && <Territorio onNavigate={go} />}
-      {route.view === 'reportes' && <ReportesDirectivosV2 />}
+      {route.view === 'reportes' && <ReportesDirectivosV3 />}
       {route.view === 'fuentes' && <Fuentes />}
       {route.view === 'metodologia' && <Metodologia />}
       {route.view === 'administracion' && (
