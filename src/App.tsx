3364121852708b465db5db038c4ad154de5e1de5
smoralesm07-source <smoralesm07@gmail.com>
2026-09-16 13:15:31 -0300

    feat: mount report studio view

--- src/App.tsx
+++ src/App.tsx
@@ -15,6 +15,7 @@ import { Entidades } from './views/Entidades';
 import { EntityExpediente } from './views/EntityExpediente';
 import { Fuentes } from './views/Fuentes';
 import { Territorio } from './views/Territorio';
+import { Reportes } from './views/Reportes';
 import { Metodologia } from './views/Metodologia';
 import { Administracion } from './views/Administracion';
 import './styles/territory-commune-typography.css';
@@ -73,6 +74,7 @@ function Routed({ session, role }: { session: Session; role: AtlasRole }) {
         </>
       )}
       {route.view === 'territorio' && <Territorio onNavigate={go} />}
+      {route.view === 'reportes' && <Reportes />}
       {route.view === 'fuentes' && <Fuentes />}
       {route.view === 'metodologia' && <Metodologia />}
       {route.view === 'administracion' && (
